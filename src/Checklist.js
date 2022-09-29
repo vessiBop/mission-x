fb94e3172810c1f13080297838e4ce9d9f67a06a
Silvestre Fernandez <rasa356@outlook.com>
2022-09-29 14:51:51 +1300

    updating checklist

--- src/Checklist.js
+++ src/Checklist.js
@@ -1,2 +1,8 @@
 // Create parent div for entire body
-// Create navbar, sectioned in 
+// Create navbar, sectioned in three parts, left, center, and right
+// Create first content div @ 1920x1080px
+// Within this div I will have two sections. Top and bottom, about 40% to 60% flex basis.
+// Next div will contain some images and a title
+// Next section will be written with lots of JS, having a section that changes the text with 4 different outputs depending on the button pressed
+// Last section of content divs will be an enquire/sign up information area
+// Lastly, create the footer
