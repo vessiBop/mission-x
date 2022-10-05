54940306d1b06b8eb4d031741cbd4b7cadfb156d
djwugee <djwugee@gmail.com>
2022-10-06 09:42:20 +1300

    Implimenting Silvestre's NavBar

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import './Fonts.css';
+
+// Navbar code, check Navbar.css file for the CSS
+
+export default function Navbar() {
+    return (
+        <><div className="navbar-parent-div">
+            <div className="left-side-child-of-navbar"></div>
+            <div className="center-child-of-navbar">
+                <p className="home-button">HOME</p>
+                <p className="features-button">FEATURES</p>
+                <p className="teachers-button">TEACHERS</p>
+            </div>
+            <div className="right-side-child-of-navbar">
+                <p className="user-circle-logo"></p>
+                <p className="features-and-login-text">FEATURES | LOGIN</p>
+                <p className="lang-button">LANG</p>
+                <p className="kiwi-flag"></p>
+                <p className="maori-flag"></p>
+            </div>
+        </div></>
+    )
+}
