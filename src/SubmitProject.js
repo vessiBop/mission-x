3dd1b5d6aef90eaf60fa7d7f643ae9e6fb14dbf2
djwugee <djwugee@gmail.com>
2022-10-06 11:54:59 +1300

    Setting up Upload/ShowTeacher Container

--- src/SubmitProject.js
+++ src/SubmitProject.js
@@ -1,12 +1,12 @@
 import './Fonts.css';
 
-// Code for the section below the navbar. Check Second-section.css
+// Code for the Upload Screenshot and Show Teacher section
 
 export default function UploadScreen() {
     return (
         <>
-            <div className="upload-parent-div">
-               
+            <div className="upload-parent-container">
+                <div className="upload-split-div">Screenshot upload / Show Teacher div</div>
             </div>
         </>
     )
