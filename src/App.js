91cbc05267199903c88bbc73b9fcfaa18e72a81f
Salman Khan <salmanakhan33@yahoo.co.nz>
2022-10-06 13:21:04 +1300

    cleaned app page

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import TeacherProfile from "./TeacherProfile/TeacherProfile";
-import HelpRequest from "./HelpRequest/HelpRequest";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
