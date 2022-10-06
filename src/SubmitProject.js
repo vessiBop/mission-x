1bcbf2b7ee0cde79b91168e7016fdcb31e801bb3
djwugee <djwugee@gmail.com>
2022-10-06 17:05:10 +1300

    Added The 'Submit Photo' text

--- src/SubmitProject.js
+++ src/SubmitProject.js
@@ -10,9 +10,9 @@ export default function UploadScreen() {
         <>
             <div className="upload-parent-container">
                 <div className="upload-split-div">
-                    
                     <img styles={imageStyles} className="screenshot-img" src={ScreenShotImg} alt="ScreenShot" />
-                    
+                    <div className="submit-text1">Submit project photo</div>
+                    <div className="submit-text2">After completing your project, take a screenshot and upload it here</div>
                 </div>
             </div>
         </>
