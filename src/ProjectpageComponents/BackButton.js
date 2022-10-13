9e1de9c4dc2e080c92871830e8e7df2f6ab57de9
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-10-13 21:57:36 +1300

    add programming tobackbutton

--- src/ProjectpageComponents/BackButton.js
+++ src/ProjectpageComponents/BackButton.js
@@ -1,10 +1,26 @@
-
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './BackButton.css';
 function BackButton(){
+    const [backButton, setBackButton] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll",() =>{
+            if(window.scrollY >200) {
+                setBackButton(true)
+            } else{
+                setBackButton(false)
+            }
+        })
+    }, [])
+    const scrollup = () =>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
     return(
-        <div>
-            <button className='btn'>Back To Top</button>
-        </div>
+        backButton && <button className='btn' onClick={scrollup}>Back To Top</button>
+
     );
 }
 export default BackButton;
